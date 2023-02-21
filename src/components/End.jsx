7598258a63288a8391f70822dc9883d6a0540dea
KelvinQiu <kelvinqiu802@outlook.com>
2023-02-21 22:23:45 +0800

    style: display word

--- src/components/End.jsx
+++ src/components/End.jsx
@@ -5,7 +5,7 @@ function End({ forget, overtime }) {
     <div className='end'>
       <h1>Overtime</h1>
       {overtime.map((w) => (
-        <h2 key={w.word}>{`${w.word}\t${w.time.toFixed(1)}s`}</h2>
+        <h2 key={w.word}>{w.word}</h2>
       ))}
       <h1>Forget</h1>
       {forget.map((w) => (
