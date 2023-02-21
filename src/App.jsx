93e9e6c77e23ded8124a8793e516b478962862e2
KelvinQiu <kelvinqiu802@outlook.com>
2023-02-21 12:53:07 +0800

    feat: phases

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import React, { useState } from 'react';
 import WordList from './components/WordList';
 import Config from './components/Config';
 
+const PHASE = {
+  prepare: 0,
+  start: 1,
+  end: 2,
+};
+
 function App() {
+  const [phase, setPhase] = useState(PHASE.prepare);
   const [selectedLst, setSelectedLst] = useState(null);
   const [config, setConfig] = useState({
     timeLimit: '5',
@@ -14,8 +21,19 @@ function App() {
   return (
     <div className='App'>
       <h1>{`<Word Race>`}</h1>
-      <WordList lst={selectedLst} setLst={setSelectedLst} />
-      <Config config={config} setConfig={setConfig} />
+      {phase == PHASE.prepare && (
+        <>
+          <WordList lst={selectedLst} setLst={setSelectedLst} />
+          <Config config={config} setConfig={setConfig} />
+          <button
+            className='start'
+            disabled={selectedLst ? false : true}
+            onClick={() => setPhase(PHASE.start)}
+          >
+            START
+          </button>
+        </>
+      )}
     </div>
   );
 }
