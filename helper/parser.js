a59cdaf2a1705b2d588908d6e758348fa2d5b79d
KelvinQiu <kelvinqiu802@outlook.com>
2023-02-21 13:10:25 +0800

    feat: update word lists

--- helper/parser.js
+++ helper/parser.js
@@ -21,75 +21,7 @@ function parseToArray(source) {
   return result;
 }
 
-const source = `- _conservation_
-- _enrol_
-- **summit**
-- specialize in
-- _resident_
-- **radical**
-- rag
-- **prescribe**
-- _manner_
-- beneath
-- _recreation_
-- procession
-- tackle
-- resort n/v
-  - resort to
-- broom
-- recruit
-- superb
-- bloody
-- fierce
-- enquire
-- _emit_
-- _superficial_
-- drip
-- sole
-- **folk**
-- enclose
-- _gamble_
-- loophole
-- **ornament**
-- paradox
-- awe
-- bail
-- _bold_
-- so-called
-- superior
-- damn
-- erroneous
-- **fatal**
-- **excursion**
-- _ignorant_
-- county
-- condense
-- asset
-- rear
-  - bring up the rear
-- hollow
-- charter
-- astonish
-- cabbage
-- **condemn**
-- surplus
-- **eliminate**
-  - eliminate the possibility
-- _alike_
-- _phenomenon_
-- curtain
-- bystander
-- **bait**
-- extinguish
-- federation
-- grin
-- **hatch**
-  - hatch out
-- irrational
-- layoff
-- _norm_
-- **decisive**
-  - play a decisive role in`;
+const source = ``;
 
 const result = parseToArray(source);
 console.log(result);
