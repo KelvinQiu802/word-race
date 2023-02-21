b451265eaef880dcc17b45c3b2f19975041381c9
KelvinQiu <kelvinqiu802@outlook.com>
2023-02-21 12:30:32 +0800

    feat: select word list

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
+import WordList from './components/WordList';
 
 function App() {
-  return <div className='App'>Hello World</div>;
+  const [selectedLst, setSelectedLst] = useState(null);
+  return (
+    <div className='App'>
+      <h1>{`<Word Race>`}</h1>
+      <WordList lst={selectedLst} setLst={setSelectedLst} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/WordList.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Lists from '../Words';
+
+const CET4 = Lists['CET-4'];
+const TOEFL = Lists.TOEFL;
+
+function WordList({ lst, setLst }) {
+  return (
+    <div className='word-lst'>
+      <h2>CET-4</h2>
+      {Object.keys(CET4).map((title) => (
+        <h3
+          key={title}
+          style={{ color: lst == title ? 'blue' : 'black' }}
+          onClick={() => setLst(title)}
+        >
+          {title}
+        </h3>
+      ))}
+      <h2>TOEFL</h2>
+      {Object.keys(TOEFL).map((title) => (
+        <h3
+          key={title}
+          style={{ color: lst == title ? 'blue' : 'black' }}
+          onClick={() => setLst(title)}
+        >
+          {title}
+        </h3>
+      ))}
+    </div>
+  );
+}
+
+export default WordList;
