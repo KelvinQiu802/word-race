f8528e43fde46a1e1e84168a94f01ee0887c33e4
KelvinQiu <kelvinqiu802@outlook.com>
2023-02-21 12:56:33 +0800

    feat: selected style

--- src/components/WordList.jsx
+++ src/components/WordList.jsx
@@ -5,13 +5,18 @@ const CET4 = Lists['CET-4'];
 const TOEFL = Lists.TOEFL;
 
 function WordList({ lst, setLst }) {
+  const selectedStyle = {
+    color: 'blue',
+    textDecorationLine: 'underline',
+  };
+
   return (
     <div className='word-lst'>
       <h2>CET-4</h2>
       {Object.keys(CET4).map((title) => (
         <h3
           key={title}
-          style={{ color: lst == title ? 'blue' : 'black' }}
+          style={title == lst ? selectedStyle : {}}
           onClick={() => setLst(title)}
         >
           {title}
